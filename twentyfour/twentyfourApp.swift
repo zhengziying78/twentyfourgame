b17c95455cc260712abcd4ec979f8641c0fbc0a1
Ziying Zheng <zhengziying@Ziyings-MacBook-Pro.local>
2025-05-28 07:13:18 -0700

    do not rotate

--- twentyfour/twentyfourApp.swift
+++ twentyfour/twentyfourApp.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 @main
-struct TwentyFourApp: App {
+struct twentyfourApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @StateObject private var gameManager = GameManager.shared
+    
+    init() {
+        // Force portrait orientation
+        UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
+        AppDelegate.orientationLock = .portrait
+    }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(gameManager)
+        }
+    }
+}
+
+// App Delegate to handle orientation lock
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+}
+
+extension twentyfourApp {
+    static func setOrientation(_ orientation: UIInterfaceOrientationMask) {
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+            windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: orientation))
         }
     }
 }
